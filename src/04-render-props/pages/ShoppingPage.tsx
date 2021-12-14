fbe2a3ff5ea9ad07efa6fc8259727913caf2b2f2
Alejoboga20 <alejoboga20@gmail.com>
2021-12-14 16:14:17 -0300

    Seting basic component

--- src/04-render-props/pages/ShoppingPage.tsx
+++ src/04-render-props/pages/ShoppingPage.tsx
@@ -1,59 +1,20 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 import { products } from '../data/products';
-import { useShoppingCart } from '../hooks/useShoppingCart';
 import '../styles/custom-styles.css';
 
-export const ShoppingPage = () => {
-	const { onProductCountChange, shoppingCart } = useShoppingCart();
+const product = products[0];
 
+export const ShoppingPage = () => {
 	return (
 		<div>
 			<h1>Shopping Store</h1>
 			<hr />
 
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					flexWrap: 'wrap',
-				}}
-			>
-				{products.map((product) => (
-					<ProductCard
-						key={product.id}
-						product={product}
-						className='bg-dark text-white'
-						onChange={onProductCountChange}
-						value={shoppingCart[product.id]?.count || 0}
-					>
-						<ProductImage className='custom-image' />
-						<ProductTitle className='text-bold' />
-						<ProductButtons className='custom-buttons' />
-					</ProductCard>
-				))}
-			</div>
-
-			<div className='shoping-cart'>
-				{Object.entries(shoppingCart).map(([key, product]) => (
-					<ProductCard
-						key={key}
-						product={product}
-						className='bg-dark text-white'
-						style={{ width: '100px' }}
-						onChange={onProductCountChange}
-						value={product.count}
-					>
-						<ProductImage className='custom-image' />
-						<ProductButtons
-							className='custom-buttons'
-							style={{
-								display: 'flex',
-								justifyContent: 'center',
-							}}
-						/>
-					</ProductCard>
-				))}
-			</div>
+			<ProductCard key={product.id} product={product} className='bg-dark text-white'>
+				<ProductImage className='custom-image' />
+				<ProductTitle className='text-bold' />
+				<ProductButtons className='custom-buttons' />
+			</ProductCard>
 		</div>
 	);
 };
